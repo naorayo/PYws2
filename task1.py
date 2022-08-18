de862006657fc3fa4180e56756bfcb45ab7a9bad
max <naorayo@yandex.ru>
2022-08-18 19:27:01 +0800

    Tasks update

--- task1.py
+++ task1.py
@@ -1,23 +1,10 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
 
 
-# Запрашиваем ввод числа и проверяем что введено число.
-def InputNumbers(inputText):
-    is_OK = False
-    while not is_OK:
-        try:
-            number = float(input(f"{inputText}"))
-            is_OK = True
-        except ValueError:
-            print("Вводить необходимо числа!")
-    return number
-
-
 """ Вычисляем сумму цифр в числе. Каждый элемент введенного числа
 	(кроме десятичного разделителя) преобразовываем в int и
 	складываем с предыдущим значением.
 """
-
 
 def addi(num):
     sum = 0
@@ -26,5 +13,5 @@ def addi(num):
             sum += int(i)
     return sum
 
-num = InputNumbers("Введите число: ")
+num = float(input("Введите число: "))
 print(f"Сумма цифр = {addi(num)}")
