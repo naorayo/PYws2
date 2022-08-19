d50c40fc629be1c9c0f94b3d0faab326c0dc4f26
max <naorayo@yandex.ru>
2022-08-19 18:50:33 +0800

    "#seq = 2 * count - 1" deleted

--- task4-2.py
+++ task4-2.py
@@ -5,7 +5,6 @@ count = 1
 need_num = int(input('Введи количество элементов в списке: '))
 while count <= need_num:
 	seq = round((count**2 - e**2 - pi**3 + count**3), 2)
-#	seq = 2 * count - 1
 	numlist.append(seq)
 	count += 1
 print(numlist)
